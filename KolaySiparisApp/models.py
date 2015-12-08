b8989c7602aec785a45abc1735a63287c16b5fe4
alimertcan <tekin.mertcan@yahoo.com>
2015-12-09 00:12:42 +0200

    Update models.py

--- KolaySiparisApp/models.py
+++ KolaySiparisApp/models.py
@@ -2,4 +2,15 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class food(models.Model):
+	name= models.CharField(max_length=255)
+	fiyat=models.PositiveIntegerField()
+	belongsrestaurant=models.CharField(max_length=255)
+	def __unicode__(self):
+      		return "name : %s , fiyat : %s , belongsrestaurant : %s" % (self.name,self.fiyat,self.belongsrestaurant)
+class restaurant(models.Model):
+	name= models.CharField(max_length=255)
+	location=models.CharField(max_length=255)
+	belongsfood=models.CharField(max_length=255)
+	def __unicode__(self):
+      		return "name : %s , location : %s , belongsfood : %s" % (self.name,self.fiyat,self.belongsfood)
