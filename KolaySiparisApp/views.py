9a6985855fb0c1a7ce0d95ea14688ae3b9bd23f6
alimertcan <tekin.mertcan@yahoo.com>
2015-12-09 00:38:58 +0200

    Update views.py

--- KolaySiparisApp/views.py
+++ KolaySiparisApp/views.py
@@ -6,4 +6,4 @@ def listfood(request):
 	return render_to_response('listfood.html',{'food':food.object.all()})
 def listres(request):
 	app_label = 'app_model_belongs_to_restaurant'
-	return render_to_response('listres.html',{'food':food.object.all()})
+	return render_to_response('listres.html',{'restaurant':restaurant.object.all()})
