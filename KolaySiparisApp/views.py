4d2f518778b24bd81562355de1b8ea44c66ac12d
alimertcan <tekin.mertcan@yahoo.com>
2015-12-09 00:13:13 +0200

    Update views.py

--- KolaySiparisApp/views.py
+++ KolaySiparisApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def listfood(request):
+	app_label = 'app_model_belongs_to_food'
+	return render_to_response('listfood.html',{'food':food.object.all()})
+def listres(request):
+	app_label = 'app_model_belongs_to_restaurant'
+	return render_to_response('listres.html',{'food':food.object.all()})
